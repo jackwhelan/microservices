516874285f2f3e1af4b85ff5dc5dbb85966f7e75
Jack Whelan <jackw97@live.ie>
2021-11-14 19:22:12 +0000

    Added ConfigHandler and primitive Mongo Adapter

--- data-access/src/adapters/api_routes.py
+++ data-access/src/adapters/api_routes.py
@@ -1,15 +1,39 @@
 '''
 API Routes for the Data-Access microservice.
 '''
+import os
 from flask.blueprints import Blueprint
+from flask import request
 
-from src.use_cases.generate_greeting import greet_person
+from src.adapters.mongo import MongoAdapter
+from src.etc.config import ConfigHandler
+
+config = ConfigHandler()
+
+database_adapter = {
+    'mongodb': MongoAdapter
+}
+
+db = database_adapter[config['database_type']](
+    os.getenv('DATABASE_HOST'),
+    os.getenv('DATABASE_PORT'),
+    os.getenv('DATABASE_USER'),
+    os.getenv('DATABASE_PASS')
+)
 
 api_routes = Blueprint('api_routes', __name__)
 
-@api_routes.route('/name/<name>')
-def greet_by_name(name):
+@api_routes.route('/<database>/<collection>', methods = ['GET', 'POST'])
+def insert(database, collection):
     '''
-    Test function to lay down some boilerplate architecture.
+    API Route to insert data into a database.
     '''
-    return greet_person(name)
+    db.connect(database)
+    if request.method == 'GET':
+        if request.args.get('id') is not None:
+            return db.find_by_id(database, collection, request.args.get('id'))
+        else:
+            print('No id passed with request')
+            return db.find_by_id(database, collection)
+    elif request.method == 'POST':
+        return db.insert(database, collection, request.form)
