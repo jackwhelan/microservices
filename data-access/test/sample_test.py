fbe60b57b5979c5948dbc10f3fe2337dad2d288f
Jack Whelan <jackw97@live.ie>
2022-06-06 19:20:30 +0100

    Fixing pytest issues

---
+++ data-access/test/sample_test.py
@@ -0,0 +1,14 @@
+"""
+Placeholder module with a sample test.
+"""
+def add(value_one, value_two):
+    """
+    Addition function to add two numbers.
+    """
+    return value_one + value_two
+
+def test_add():
+    """
+    Sample test for the above add function.
+    """
+    assert add(5, 10) == 15
